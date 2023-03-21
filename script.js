b7eeb9d864da988395e24a97bdd21ef6fb7cc931
Sandesh N <122511902+Sandesh64@users.noreply.github.com>
2023-03-22 00:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Header toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['','Front-End Developer....!','Freelancer....!','UI/UX Designer....!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
+
+
+
+//Get all links
+let navLinks = document.querySelectorAll('nav ul li a')
+// Get All Sections
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
